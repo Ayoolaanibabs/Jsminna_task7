fb56aabf43b21e3e6a0cdaf157f3e85acb373f43
Ayoolaanibabs <64548560+Ayoolaanibabs@users.noreply.github.com>
2020-10-23 22:35:57 +0100

    Update Palindrome.js

--- Palindrome.js
+++ Palindrome.js
@@ -10,7 +10,7 @@ export function palindrome (string) {
  * Node js repl
  * node js repl stands for read, evaluate,print and loop. It is a prompt in the command line that can be used to write 
  * javascript code, evaluate the result of the code, print the result and then give a new box to write more code. 
- * The node js repl can be called on the command line by typing the "node" instruction, to quit thr prompt totally, the .exit command is used.
+ * The node js repl can be called on the command line by typing the "node" instruction, to quit the prompt totally, the ".exit" command is used.
  * In the node js repl, varibales can also be declared and values stored in them but after quitting the session, the variables are deleted fr4om the memory.
- * the node repl is useful when you want to test out code in which you want to see the result quickly.
+ * The node repl is useful when you want to test out code in which you want to see the result quickly.
  */
